ce413e1e93395bc4f31863a75743b57707ed0de9
G00Z_G00Z <edygomezgg4@gmail.com>
2022-10-03 19:29:54 -0500

    Corrected Display

--- src/Display.cpp
+++ src/Display.cpp
@@ -1,24 +1,22 @@
 #include <Display.h>
 #include <Arduino.h>
 
-namespace Display
+static void printPowerInfo(Display::LCD &lcd, float ledNum, float power)
 {
-    void clearAndResetCursor(Display::LCD &lcd)
-    {
-        lcd.clear();
-        lcd.home();
-    }
+    lcd.print("Led" + String(ledNum) + ": " + String(power) + "W");
+}
 
-    void printPowerLeds(Display::LCD &lcd, float pow1, float pow2)
-    {
-        clearAndResetCursor(lcd);
-        printPowerInfo(lcd, 1, pow1);
-        lcd.setCursor(0, 1);
-        printPowerInfo(lcd, 2, pow2);
-    }
+void Display::clearAndResetCursor(Display::LCD &lcd)
+{
+    lcd.clear();
+    lcd.home();
+}
 
-    static void printPowerInfo(Display::LCD &lcd, float ledNum, float power)
-    {
-        lcd.print("Led" + String(ledNum) + ": " + String(power) + "W");
-    }
-};
+void Display::printPowerLeds(Display::LCD &lcd, float pow1, float pow2)
+{
+    using namespace Display;
+    clearAndResetCursor(lcd);
+    printPowerInfo(lcd, 1, pow1);
+    lcd.setCursor(0, 1);
+    printPowerInfo(lcd, 2, pow2);
+}
