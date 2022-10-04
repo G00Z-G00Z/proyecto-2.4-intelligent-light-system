c6c543a3660a0ce185a3704417ffd2b9a96b3119
G00Z_G00Z <edygomezgg4@gmail.com>
2022-10-03 19:31:09 -0500

    Finished functions in display

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
+#include <Display.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const int rs = 40,
+          en = 41,
+          d4 = 42,
+          d5 = 43,
+          d6 = 44,
+          d7 = 45;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("Hello world");
+  Serial.println("Hello world!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Display::printPowerLeds(lcd, 1.0f, 55.5f);
+  delay(300);
 }
