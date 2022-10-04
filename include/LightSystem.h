e583a1f686401ff01518bde513362e2d7b58a8b2
G00Z_G00Z <edygomezgg4@gmail.com>
2022-10-03 21:47:01 -0500

    Light system

--- include/LightSystem.h
+++ include/LightSystem.h
@@ -1,17 +1,50 @@
 #pragma once
 #include <RBD_LightSensor.h>
+#include <MotorPWM.h>
 
 namespace LightSystem
 {
+
+    /**
+     * @brief Intelligente light systema that regulates a room light
+     * For the light sensors, 0 -> darkess, 1023 -> Light
+     */
     class IntelligentLightSystem
     {
+
+        struct LightValues
+        {
+            int inside;
+            int outside;
+            int ideal;
+        };
 
         RBD::LightSensor inside;
         RBD::LightSensor outside;
 
+        LightValues values;
+
+        Driver::MotorPWM *motor;
+
+        int currentDuty;
+
+        /**
+         * @brief Gets the sum of the inside and the outside values, and
+         * returns a 1 the light source must increase, 0 if its good, and -1 if
+         * it should decrease
+         *
+         * @return int
+         */
+        int getModifyGradient();
+
+        void updateValues();
+
     public:
-        IntelligentLightSystem(int pinInside, int pinOutside);
+        IntelligentLightSystem(int pinInside, int pinOutside, Driver::MotorPWM &motor, int idealLightValue);
 
         void printSerialValues();
+
+        void update();
     };
+
 };
--- src/LightSystem.cpp
+++ src/LightSystem.cpp
@@ -1,15 +1,66 @@
 #include <LightSystem.h>
+#include <math.h>
 #include <Arduino.h>
 #include <RBD_LightSensor.h>
+#include <MotorPWM.h>
 
 using namespace LightSystem;
 
-IntelligentLightSystem::IntelligentLightSystem(int pinInside, int pinOutside) : inside(pinInside), outside(pinOutside)
+static const int threshold = 5;
+static const int changeFactor = 5;
+
+IntelligentLightSystem::IntelligentLightSystem(int pinInside, int pinOutside, Driver::MotorPWM &motor, int idealLightValue)
+    : inside(RBD::LightSensor(pinInside)),
+      outside(RBD::LightSensor(pinOutside)),
+      motor(&motor),
+      currentDuty(0)
 {
+    values.ideal = idealLightValue;
+    motor.setDutyCycle(currentDuty);
+    motor.forward();
 }
 
 void IntelligentLightSystem::printSerialValues()
 {
-    Serial.println("Outside: " + String(outside.getRawValue()));
-    Serial.println("Inside: " + String(inside.getRawValue()));
+    Serial.println("Outside: " + String(values.outside));
+    Serial.println("Inside: " + String(values.inside));
+}
+
+void IntelligentLightSystem::updateValues()
+{
+    values.inside = inside.getRawValue();
+    values.outside = outside.getRawValue();
+}
+
+int IntelligentLightSystem::getModifyGradient()
+{
+    int difference = (values.ideal - values.inside + values.outside);
+    bool isPositive = difference > 0;
+
+    if (abs(difference) <= threshold)
+    {
+        return 0;
+    }
+
+    return isPositive ? 1 : -1;
+}
+
+void IntelligentLightSystem::update()
+{
+    updateValues();
+    int gradient = getModifyGradient();
+    int change = changeFactor * gradient;
+    int nextDuty = change + currentDuty;
+
+    Serial.println("Calculating gradient:" + String(gradient) + "/" + String(currentDuty) + "/" + String(nextDuty));
+
+    // Check bounds
+    if (nextDuty < 0 or 255 < nextDuty)
+    {
+        return;
+    }
+
+    currentDuty = nextDuty;
+
+    motor->setDutyCycle(currentDuty);
 }
