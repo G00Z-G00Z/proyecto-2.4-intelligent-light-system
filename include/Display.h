8019b0a28a92a9514e91e3f364fe109cb08287d0
G00Z_G00Z <edygomezgg4@gmail.com>
2022-10-03 19:23:33 -0500

    Changed pointers -> reference

--- include/Display.h
+++ include/Display.h
@@ -6,8 +6,8 @@ namespace Display
 
     using LCD = LiquidCrystal;
 
-    void clearAndResetCursor(LCD *lcd);
+    void clearAndResetCursor(LCD &lcd);
 
-    void printPowerLeds(LCD *lcd, float pow1, float pow2);
+    void printPowerLeds(LCD &lcd, float pow1, float pow2);
 
 };
--- src/Display.cpp
+++ src/Display.cpp
@@ -3,22 +3,22 @@
 
 namespace Display
 {
-    void clearAndResetCursor(Display::LCD *lcd)
+    void clearAndResetCursor(Display::LCD &lcd)
     {
-        lcd->clear();
-        lcd->home();
+        lcd.clear();
+        lcd.home();
     }
 
-    void printPowerLeds(Display::LCD *lcd, float pow1, float pow2)
+    void printPowerLeds(Display::LCD &lcd, float pow1, float pow2)
     {
         clearAndResetCursor(lcd);
         printPowerInfo(lcd, 1, pow1);
-        lcd->setCursor(0, 1);
+        lcd.setCursor(0, 1);
         printPowerInfo(lcd, 2, pow2);
     }
 
-    static void printPowerInfo(Display::LCD *lcd, float ledNum, float power)
+    static void printPowerInfo(Display::LCD &lcd, float ledNum, float power)
     {
-        lcd->print("Led" + String(ledNum) + ": " + String(power) + "W");
+        lcd.print("Led" + String(ledNum) + ": " + String(power) + "W");
     }
 };
