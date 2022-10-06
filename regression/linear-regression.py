28a0bd5ebd14c45f58a9af1b4fb960e064984eb0
G00Z_G00Z <edygomezgg4@gmail.com>
2022-10-05 19:52:47 -0500

    Changed table to scatter

--- regression/linear-regression.py
+++ regression/linear-regression.py
@@ -10,19 +10,19 @@ def main():
     sensor_table = pd.read_csv(SENSOR_DATA_FILENAME)
     plt.figure(1)
     sensor_table  = sensor_table.apply(pd.to_numeric, args=('coerce',))
-    sensor_table.plot( x="vol", y="current",grid=True, title="current vs input voltage")
+    sensor_table.plot(kind="scatter", x="vol", y="current",grid=True, title="current vs input voltage", label="data")
     distance = sensor_table.loc[:, "current"] .to_numpy().reshape(-1,1)
     voltage = sensor_table.loc[:, "vol"] .to_numpy().reshape(-1,1)
 
     # Regresion 
-    polyModel = PolynomialFeatures(degree = 2, include_bias=False)
-    poly_features_train = polyModel.fit_transform(voltage)
+    poly_model = PolynomialFeatures(degree = 2, include_bias=False)
+    poly_features_train = poly_model.fit_transform(voltage)
     poly_model = LinearRegression()
     poly_model.fit(poly_features_train, distance)
 
     distance_predicts = poly_model.predict(poly_features_train)  
-    R2 = poly_model.score(poly_features_train, distance)
-    print(f"R2 = {R2}")
+    R_2 = poly_model.score(poly_features_train, distance)
+    print(f"R2 = {R_2}")
     coeffs = poly_model.coef_
     print(f"Coeefs = {coeffs}")
     print(f"inter = {poly_model.intercept_}")
